af5d42339ba4b9c8b9ae2b5d39a3c969a2aa442e
Aijaz Ansari <aijaz@users.noreply.github.com>
2023-04-19 05:06:51 -0600

    Initial Commit

---
+++ FallDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FallDetector
+//
+//  Created by Aijaz Ansari on 4/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
